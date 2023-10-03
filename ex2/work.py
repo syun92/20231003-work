d428d5fedead228e1b7693fb5f2c68cc2e3a1098
HASHIMOTO Syunpei <saltfish2229aks@gmail.com>
2023-10-03 12:29:54 +0900

    問題2の提出

--- ex2/work.py
+++ ex2/work.py
@@ -1,8 +1,37 @@
 from math import ceil
 
+""" タクシーの運賃計算
+    タクシーの運賃を計算するコードを作成せよ
+    * 初乗り運賃は 1700m までは610円とする
+    * 1700mを超えるとその後は 315m毎に 80円増えるものとする
+    * 引数として走行距離(整数でm単位)が渡されるものとする
+    * 戻り値は金額(整数値)とする
+    * 1mでも超えれば80円単位でかかるものとする
+    * 0mおよびマイナスの場合はNoneを返す
+    # 初乗りの距離までは定額610円
+    min = 610 # 初乗り運賃
+    assert calc_account(1000) == min
+    assert calc_account(1700) == min
+    assert calc_account(1701) == 690 # 1mでも超えれば増える
+    assert calc_account(2015) == 690
+    assert calc_account(2016) == 770
+    assert calc_account(0) == None
+    assert calc_account(-10) == None
+"""
+
 def calc_account(m):
-    # 実装は入れていません、自分で入れてください
-    pass
+    if m <= 0:
+        # 0mおよびマイナスの場合
+        return None
+    elif 1700 < m:
+        # 1700を超えた場合
+        extension_kyori = m - 1700
+        extension_round = ceil(extension_kyori / 315)
+        extension_money = extension_round * 80 + 610
+        return extension_money
+    else:
+        # 1700までの場合
+        return 610
 
 if __name__ == "__main__":
     from argparse import ArgumentParser
